9abfb6443dd3ad1a8c8b8aa6555e1ed13c3097af
fatihcandev <fatihcan.social@gmail.com>
2020-12-07 10:10:15 +0300

    custom path alias config

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,11 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          '@tests': ['src/tests'],
+          '@tests': 'src/tests',
           '@assets': 'src/assets',
+          '@theme': 'src/theme',
           '@types': 'src/types',
+          '@context': 'src/context',
           '@constants': 'src/constants',
           '@utils': 'src/utils',
           '@navigation': 'src/navigation',
