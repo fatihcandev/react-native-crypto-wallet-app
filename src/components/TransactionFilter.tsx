1d7f694ae38b1d01a3552de7179d25c86a65c0e6
fatihcandev <fatihcan.social@gmail.com>
2020-12-08 20:24:49 +0300

    create TransactionFilter component

---
+++ src/components/TransactionFilter.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import Box from './Box';
+import ButtonContainer from './ButtonContainer';
+import Icon from './Icon';
+import StyledText from './StyledText';
+
+interface ITransactionFilterProps {
+  type: 'transactionType' | 'coin' | 'date';
+  filterAmount: number;
+  onPress: () => void;
+}
+
+const TransactionFilter: React.FC<ITransactionFilterProps> = ({ type, filterAmount, onPress }) => {
+  const isSelected = filterAmount > 0;
+
+  const getFilterText = () => {
+    switch (type) {
+      case 'transactionType':
+        return 'Transaction type';
+      case 'coin':
+        return 'Coin';
+      case 'date':
+        return 'Date';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <ButtonContainer
+      justifyContent="space-between"
+      height={35}
+      backgroundColor={isSelected ? 'accent' : 'actionButton'}
+      alignSelf="flex-start"
+      {...{ onPress }}
+    >
+      <StyledText variant="paragraph" color={isSelected ? 'white' : 'paragraph'} marginRight="s">
+        {getFilterText()}
+      </StyledText>
+      {isSelected && (
+        <Box
+          width={18}
+          height={18}
+          justifyContent="center"
+          alignItems="center"
+          marginRight="s"
+          backgroundColor="white"
+          borderRadius="full"
+        >
+          <StyledText variant="fineprint" color="accent">
+            {filterAmount}
+          </StyledText>
+        </Box>
+      )}
+      <Icon name="chevronDown" color={isSelected ? 'white' : 'paragraph'} />
+    </ButtonContainer>
+  );
+};
+
+export default TransactionFilter;
