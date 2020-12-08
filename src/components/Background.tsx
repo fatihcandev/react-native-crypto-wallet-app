96cb41787e247015518a6ced42a81935fdc34527
fatihcandev <fatihcan.social@gmail.com>
2020-12-08 14:03:49 +0300

    set status bar color according to dark mode

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTheme } from '@shopify/restyle';
+
+import { Theme } from 'theme';
+import { AppContext } from 'context';
 
 import Box from './Box';
 
 const Background: React.FC = ({ children }) => {
+  const { state } = useContext(AppContext);
+  const theme = useTheme<Theme>();
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar
+        backgroundColor={theme.colors.bgPrimary}
+        barStyle={state.darkMode ? 'light-content' : 'dark-content'}
+      />
       <Box flex={1} backgroundColor="bgPrimary">
         {children}
       </Box>
